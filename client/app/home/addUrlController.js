abefaebfe030ccdbd58c6de1ba9d48602a9db7a7
John Murphy <chiefsmurph@gmail.com>
2015-06-20 14:25:06 -0700

    Feature: created urls array on scope

--- client/app/home/addUrlController.js
+++ client/app/home/addUrlController.js
@@ -1,16 +1,4 @@
 angular.module('app.home.addUrl', [ 'ui.router'])
 .controller('addUrlController', function ($scope, $state, $http) {
 
- $scope.html = '';
-
- $scope.add = function() {
-     var loginUrl = '/api/users/addUrl';
-     console.log('urlCtrl');
-     $http.post(loginUrl, {url: $scope.url })
-       .success(function (data) {
-         console.log(data);
-         $scope.html = 'newValues';
-         $state.go('home.addUrl');
-       });
- };
 });
--- client/app/home/homeController.js
+++ client/app/home/homeController.js
@@ -1,3 +1,18 @@
 angular.module('app.home', ['app.home.addUrl', 'app.home.results', 'ui.router'])
-.controller('homeController', function ($scope, $state) {
+.controller('homeController', function ($scope, $state, $http) {
+
+   $scope.html = 'dsa';
+   $scope.urls = ['as', 'ds'];
+   console.log($scope.urls);
+   $scope.add = function() {
+       $scope.urls.push($scope.url);
+       var loginUrl = '/api/users/addUrl';
+       console.log($scope.urls);
+       $http.post(loginUrl, {url: $scope.url })
+         .success(function (data) {
+           console.log(data);
+           $scope.html = data;
+         });
+   };
+
 });
